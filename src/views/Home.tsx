1ce1f4f2212ca2f893f5cf62b5b9886dd202978c
Maksim V <maks112v@gmail.com>
2021-05-19 09:25:02 -0400

    working on ruby

--- src/views/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-interface HomePageProps {}
-
-const HomePage: FunctionComponent<HomePageProps> = ({ children }) => {
-  return <></>;
-};
-
-export default HomePage;
